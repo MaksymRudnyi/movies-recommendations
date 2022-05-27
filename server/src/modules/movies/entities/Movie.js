976a02423b95ac76a053853bd8ed7eb9e221fb58
Maksym Rudnyi <maksym.rudnyi@fiverr.com>
2022-05-27 21:05:20 +0300

    add pagination

--- server/src/modules/movies/entities/Movie.js
+++ server/src/modules/movies/entities/Movie.js
@@ -18,9 +18,17 @@ class Movie {
     }
 
     releaseDate(params) {
-        return params.format 
-            ? format(new Date(this.movie.release_date), params.format)
-            : this.movie.release_date
+        try {
+            const date = params.format 
+                ? format(new Date(this.movie.release_date), params.format)
+                : this.movie.release_date;
+
+            return date
+        } catch (e) {
+            console.error(e);
+            return this.movie.release_date;
+        }
+        
     }
 }
 
--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -2,7 +2,7 @@ const { getPopular } = require('../modules/movies');
 
 async function movies(parent, args) {
 
-    const data = await getPopular();
+    const data = await getPopular(args.page);
 
     return data;
 }
