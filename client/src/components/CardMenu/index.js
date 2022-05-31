2bbc3e4f133509ca1066a678608bf4e8b574721a
Maksym Rudnyi <maksym.rudnyi@fiverr.com>
2022-05-31 10:18:19 +0300

    menu click handler. Hide menu after click

--- client/src/components/CardMenu/index.js
+++ client/src/components/CardMenu/index.js
@@ -36,6 +36,7 @@ const CardMenu = ({ children }) => {
                 anchorEl={anchorEl}
                 open={open}
                 onClose={handleClose}
+                onClick={handleClose}
                 PaperProps={{
                     style: {
                         width: '20ch',
